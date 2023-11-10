2470f53196141e8e7dc46faf46a29ccea506ba76
Joe Koop <joe@joekoop.com>
2023-11-10 12:48:15 -0600

    compute_tree.c: change board.spaces to a 1d array

--- compute_tree.c
+++ compute_tree.c
@@ -8,10 +8,8 @@
 #define CONT 3
 #define DRAW 4
 
-#define SPACE spaces[i][j]
-
 typedef struct {
-	uint_fast8_t spaces[3][3];
+	uint_fast8_t spaces[9];
 	uint_fast8_t end_condition;
 	uint_fast8_t turn_offset;
 } board;
@@ -19,35 +17,35 @@ typedef struct {
 // who won?
 uint_fast8_t get_end_condition(const board *board) {
 	// check for win
-	if (board->spaces[0][0] != CLEAR && board->spaces[0][0] == board->spaces[0][1] &&
-	    board->spaces[0][1] == board->spaces[0][2])
-		return board->spaces[0][0];
-	if (board->spaces[1][0] != CLEAR && board->spaces[1][0] == board->spaces[1][1] &&
-	    board->spaces[1][1] == board->spaces[1][2])
-		return board->spaces[1][0];
-	if (board->spaces[2][0] != CLEAR && board->spaces[2][0] == board->spaces[2][1] &&
-	    board->spaces[2][1] == board->spaces[2][2])
-		return board->spaces[2][0];
-	if (board->spaces[0][0] != CLEAR && board->spaces[0][0] == board->spaces[1][0] &&
-	    board->spaces[1][0] == board->spaces[2][0])
-		return board->spaces[0][0];
-	if (board->spaces[0][1] != CLEAR && board->spaces[0][1] == board->spaces[1][1] &&
-	    board->spaces[1][1] == board->spaces[2][1])
-		return board->spaces[0][1];
-	if (board->spaces[0][2] != CLEAR && board->spaces[0][2] == board->spaces[1][2] &&
-	    board->spaces[1][2] == board->spaces[2][2])
-		return board->spaces[0][2];
-	if (board->spaces[0][0] != CLEAR && board->spaces[0][0] == board->spaces[1][1] &&
-	    board->spaces[1][1] == board->spaces[2][2])
-		return board->spaces[0][0];
-	if (board->spaces[2][0] != CLEAR && board->spaces[2][0] == board->spaces[1][1] &&
-	    board->spaces[1][1] == board->spaces[0][2])
-		return board->spaces[2][0];
+	if (board->spaces[0] != CLEAR && board->spaces[0] == board->spaces[1] &&
+	    board->spaces[1] == board->spaces[2])
+		return board->spaces[0];
+	if (board->spaces[3] != CLEAR && board->spaces[3] == board->spaces[4] &&
+	    board->spaces[4] == board->spaces[5])
+		return board->spaces[3];
+	if (board->spaces[6] != CLEAR && board->spaces[6] == board->spaces[7] &&
+	    board->spaces[7] == board->spaces[8])
+		return board->spaces[6];
+	if (board->spaces[0] != CLEAR && board->spaces[0] == board->spaces[3] &&
+	    board->spaces[3] == board->spaces[6])
+		return board->spaces[0];
+	if (board->spaces[1] != CLEAR && board->spaces[1] == board->spaces[4] &&
+	    board->spaces[4] == board->spaces[7])
+		return board->spaces[1];
+	if (board->spaces[2] != CLEAR && board->spaces[2] == board->spaces[5] &&
+	    board->spaces[5] == board->spaces[8])
+		return board->spaces[2];
+	if (board->spaces[0] != CLEAR && board->spaces[0] == board->spaces[4] &&
+	    board->spaces[4] == board->spaces[8])
+		return board->spaces[0];
+	if (board->spaces[6] != CLEAR && board->spaces[6] == board->spaces[4] &&
+	    board->spaces[4] == board->spaces[2])
+		return board->spaces[6];
 
 	// check for draw
-	if ((board->spaces[0][0] != CLEAR && board->spaces[0][1] != CLEAR && board->spaces[0][2] != CLEAR) &&
-	    (board->spaces[1][0] != CLEAR && board->spaces[1][1] != CLEAR && board->spaces[1][2] != CLEAR) &&
-	    (board->spaces[2][0] != CLEAR && board->spaces[2][1] != CLEAR && board->spaces[2][2] != CLEAR))
+	if ((board->spaces[0] != CLEAR && board->spaces[1] != CLEAR && board->spaces[2] != CLEAR) &&
+	    (board->spaces[3] != CLEAR && board->spaces[4] != CLEAR && board->spaces[5] != CLEAR) &&
+	    (board->spaces[6] != CLEAR && board->spaces[7] != CLEAR && board->spaces[8] != CLEAR))
 		return DRAW;
 
 	return CONT;
@@ -71,57 +69,30 @@ int main() {
 			reset_turn_offset = 0;
 		}
 		turn_offset_now = 0;
-		for (uint_fast8_t i = 0; i < 3; i++)
-			for (uint_fast8_t j = 0; j < 3; j++) {
-				if (boards[depth_now].SPACE == CLEAR) {
-					if (turn_offset_now == boards[depth_now].turn_offset) {
-						boards[depth_now].SPACE = turn_now;
-						skip_turn = 0;
-						goto did_a_turn;
-					}
-					turn_offset_now++;
+		for (uint_fast8_t i = 0; i < 9; i++)
+			if (boards[depth_now].spaces[i] == CLEAR) {
+				if (turn_offset_now == boards[depth_now].turn_offset) {
+					boards[depth_now].spaces[i] = turn_now;
+					skip_turn = 0;
+					goto did_a_turn;
 				}
+				turn_offset_now++;
 			}
 		skip_turn = 1;
 	did_a_turn:
 		boards[depth_now].end_condition = get_end_condition(&boards[depth_now]);
 
-		// // print relationship -->>
-		// printf("%i\t", depth_now);
-		// if (turn_now == WHITE) printf("WHITE\t");
-		// if (turn_now == BLACK) printf("BLACK\t");
-		// for (uint_fast8_t i = 0; i < 3; i++)
-		// 	for (uint_fast8_t j = 0; j < 3; j++)
-		// 		printf("%i", boards[depth_now - 1].SPACE);
-		// printf("\t%i\t", boards[depth_now - 1].turn_offset);
-		// if (boards[depth_now - 1].end_condition == CONT) printf("NO END");
-		// if (boards[depth_now - 1].end_condition == WHITE) printf("WHITE");
-		// if (boards[depth_now - 1].end_condition == BLACK) printf("BLACK");
-		// if (boards[depth_now - 1].end_condition == DRAW) printf("DRAW");
-		// printf("\t");
-		// for (uint_fast8_t i = 0; i < 3; i++)
-		// 	for (uint_fast8_t j = 0; j < 3; j++)
-		// 		printf("%i", boards[depth_now].SPACE);
-		// printf("\t%i\t", boards[depth_now].turn_offset);
-		// if (boards[depth_now].end_condition == CONT) printf("CONT");
-		// if (boards[depth_now].end_condition == WHITE) printf("WHITE");
-		// if (boards[depth_now].end_condition == BLACK) printf("BLACK");
-		// if (boards[depth_now].end_condition == DRAW) printf("DRAW");
-		// // <<-- print relationship
-
 		// print relationship -->>
 		if (skip_turn == 0) {
-			for (uint_fast8_t i = 0; i < 3; i++)
-				for (uint_fast8_t j = 0; j < 3; j++)
-					printf("%i", boards[depth_now - 1].SPACE);
+			for (uint_fast8_t i = 0; i < 9; i++)
+				printf("%i", boards[depth_now - 1].spaces[i]);
 			printf("\t");
 
 			if (turn_now == WHITE) printf("WHITE\t");
 			if (turn_now == BLACK) printf("BLACK\t");
 
-			for (uint_fast8_t i = 0; i < 3; i++)
-				for (uint_fast8_t j = 0; j < 3; j++)
-					printf("%i", boards[depth_now].SPACE);
+			for (uint_fast8_t i = 0; i < 9; i++)
+				printf("%i", boards[depth_now].spaces[i]);
 			printf("\t");
 
 			if (boards[depth_now].end_condition == CONT) printf("CONT");
